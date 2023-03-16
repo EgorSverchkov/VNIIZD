fb2e64a706ca87002b31abea9f15e6b5a3a9e66e
Egor Sverchkov <sverch322@gmail.com>
2023-03-16 23:53:59 +0300

    init commit

---
+++ src/main/java/ru/sverchkov/vniizd/VniizdApplication.java
@@ -0,0 +1,13 @@
+package ru.sverchkov.vniizd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VniizdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VniizdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sverchkov/vniizd/VniizdApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sverchkov.vniizd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VniizdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
